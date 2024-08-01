3c004cdba28494ea0c0c8cb16f74070b6982c980
ammarrqureshi <mamqureshi786@gmail.com>
2024-08-01 13:02:09 +0500

    button glow effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@/components/ui/button";
+import ThemeToggle from "@/components/ui/theme-toggle";
 export default function Home() {
   return (
     <main className="">
       <div>Hello world</div>
+      <ThemeToggle />
       <Button>Get Started</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="link">LInk</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="secondary">Learn More</Button>
+      <Button variant="destructive">Danger</Button>
     </main>
   );
 }
